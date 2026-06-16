967cd46c2e726890dc7b20d8be67dab14beafdf0
vinidev05 <vini06041013@gmail.com>
2026-06-16 12:11:36 -0300

    fix: corrige alias do jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 const nextJest = require("next/jest");
 
-const createJestConfig = nextJest({ dir: "./" });
+const createJestConfig = nextJest({
+  dir: "./",
+});
 
-const config = {
-  testEnvironment: "jest-environment-jsdom",
+const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
+  testEnvironment: "jest-environment-jsdom",
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
 };
 
-module.exports = createJestConfig(config);
+module.exports = createJestConfig(customJestConfig);
